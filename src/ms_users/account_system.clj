f187356e1f0ae23843281977edd5f6dd8c3b2711
German Goldenstein <german.goldenstein@gmail.com>
2015-03-09 00:02:26 -0300

    default.properties change

--- src/ms_users/account_system.clj
+++ src/ms_users/account_system.clj
@@ -1,7 +1,8 @@
 (ns ms-users.account-system
   (:require [de.otto.tesla.system :as system]
             [ms-users.signup-page :as signup-page]
-            [com.stuartsierra.component :as c])
+            [com.stuartsierra.component :as c]
+            [environ.core :refer [env]])
   (:gen-class))
 
 
@@ -11,8 +12,29 @@
              (c/using (signup-page/new-signup-page) [:routes :app-status]))
       (c/system-using {:server [:signup-page]})))
 
+(defn create-default-properties []
+  (def port (or (env :port) 8080)
+  (def text (str " status.url=/status
+              health.url=/health
+
+              ### metering
+              metering.reporter=console
+              console.interval.seconds=100
+
+              ### activate graphite like this:
+              #metering.reporter=graphite
+              #graphite.host=localhost
+              #graphite.port=2003
+              #graphite.prefix=my-app-prefix
+              #graphite.interval.seconds=60
+
+              server.port=" port)
+    (spit "resources/default.properties" text)
+  )
+
 (defn -main
   "starts up the production system."
   [& args]
+  (create-default-properties)
   (system/start-system (account-system{})))
 
