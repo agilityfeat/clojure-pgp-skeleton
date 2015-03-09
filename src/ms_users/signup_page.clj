014860bf6270fd1c38e1a9ad7e83543b8d006c73
German Goldenstein <german.goldenstein@gmail.com>
2015-03-09 19:22:03 -0300

    Login code added

--- src/ms_users/signup_page.clj
+++ src/ms_users/signup_page.clj
@@ -29,29 +29,38 @@
    public_key
   )
 
-(defn register-user [form]
+(defn register-user 
+  "Validate if an username exists. If not, then proceed to register the user."
+  [form]
       (if-not (db/get-user (:username form))
         (do 
             (db/create-user {:username (:username form) :public_key (:public_key form)})
-            true
-        )
-        false
-      )
-)
+            true)
+        false))
 
-(defn encrypt-message [form]
-  (def message (str "Welcome " (:username form) "! Your registration was successful!"))
-  (def public_key (format-public-key (:public_key form)))
-  (def encrypted-message
-            (pgp-msg/encrypt
+(defn login-user 
+  "Validate if an username exists. If not, then proceed to register the user."
+  [form]
+      (db/get-user (:username form)))
+
+(defn get-encrypted-message [message public_key]
+    (pgp-msg/encrypt
               message public_key
               :format :utf8
               :cipher :aes-256
               :compress :zip
               :armor true))
-  encrypted-message)
 
-(defn welcome-page 
+(defn encrypt-message 
+    ([form]
+      (def message (str "Welcome " (:username form) "! Your registration was successful!"))
+      (get-encrypted-message message (format-public-key (:public_key form))))
+  
+    ([form-login public-key-db]
+      (def message (str "Welcome " (:username form-login) "! Your login was successful!"))
+      (get-encrypted-message message (format-public-key public-key-db))))
+
+(defn welcome-page-signup 
    "return the welcome page encrypted with PGP"
    [body]
    (def form (getmap body))
@@ -63,18 +72,36 @@
    )
 )
 
+(defn welcome-page-login 
+   "return the welcome page encrypted with PGP"
+   [body]
+   (def form (getmap body))
+   (def loged-user (login-user form))
+   (println (str "public key -->" (:public_key loged-user)))
+   (if-not (nil? loged-user)
+      ;Register ok
+      (encrypt-message form (:public_key loged-user))
+      ;User not exists
+      "user-not-exists"
+   )
+)
+
 (defn signup-user [self body]
     {:status  200
      :headers {"Content-Type" "application/json"}
-     :body    (json/write-str (welcome-page body) )})
+     :body    (json/write-str (welcome-page-signup body) )})
 
-
+(defn signin-user [self body]
+    {:status  200
+     :headers {"Content-Type" "application/json"}
+     :body    (json/write-str (welcome-page-login body) )})
 
 (defrecord SignupPage []
   c/Lifecycle
   (start [self]
     (routes/register-routes (:routes self)
-                            [(compojure/POST "/signup" {body :body} (signup-user self (slurp body)))])
+                            [(compojure/POST "/signup" {body :body} (signup-user self (slurp body)))
+                             (compojure/POST "/login" {body :body} (signin-user self (slurp body)))])
     ;(app-status/register-status-fun (:app-status self)
       ;(fn [] (status/status-detail :example-page :ok "page is always fine")))
     self)
